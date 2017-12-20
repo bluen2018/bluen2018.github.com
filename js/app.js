6704ccfd2b97791043bf46ecce4bc6e3256d2b12
김용일 <akira76@zcode.kr>
2017-12-20 18:01:35 +0900

    다국어 지원 준비 완료 D-1

--- js/app.js
+++ js/app.js
@@ -18,18 +18,45 @@
 // 나중에 중첩 된 라우트에 대해 이야기하겠습니다.
 var routes = [
   { path: '/', component: httpVueLoader('main.vue') },
-  { path: '/about', component: httpVueLoader('about.vue') },
-  { path: '/business', component: httpVueLoader('business.vue') },
-  { path: '/contact', component: httpVueLoader('contact.vue') },
-  { path: '/project', component: httpVueLoader('project.vue') },
-  { path: '/history', component: httpVueLoader('history.vue') },
-  { path: '/history_detail/:idx', component: httpVueLoader('history_detail.vue') },
-  { path: '/project', component: httpVueLoader('project.vue') },
-  { path: '/project_1', component: httpVueLoader('project_1.vue') },
-  { path: '/project_2', component: httpVueLoader('project_2.vue') },
-  { path: '/project_3', component: httpVueLoader('project_3.vue') },
-  { path: '/project_4', component: httpVueLoader('project_4.vue') },
+  { path: '/ko', component: httpVueLoader('main.vue') },
+  { path: '/ko/about', component: httpVueLoader('about.vue') },
+  { path: '/ko/business', component: httpVueLoader('business.vue') },
+  { path: '/ko/contact', component: httpVueLoader('contact.vue') },
+  { path: '/ko/project', component: httpVueLoader('project.vue') },
+  { path: '/ko/history', component: httpVueLoader('history.vue') },
+  { path: '/ko/history_detail/:idx', component: httpVueLoader('history_detail.vue') },
+  { path: '/ko/project', component: httpVueLoader('project.vue') },
+  { path: '/ko/project_1', component: httpVueLoader('project_1.vue') },
+  { path: '/ko/project_2', component: httpVueLoader('project_2.vue') },
+  { path: '/ko/project_3', component: httpVueLoader('project_3.vue') },
+  { path: '/ko/project_4', component: httpVueLoader('project_4.vue') },
+
+  { path: '/en', component: httpVueLoader('/en/main.vue') },
+  { path: '/en/about', component: httpVueLoader('/en/about.vue') },
+  { path: '/en/business', component: httpVueLoader('/en/business.vue') },
+  { path: '/en/contact', component: httpVueLoader('/en/contact.vue') },
+  { path: '/en/project', component: httpVueLoader('/en/project.vue') },
+  { path: '/en/history', component: httpVueLoader('history.vue') },
+  { path: '/en/history_detail/:idx', component: httpVueLoader('history_detail.vue') },
+  { path: '/en/project', component: httpVueLoader('/en/project.vue') },
+  { path: '/ko/project_1', component: httpVueLoader('/en/project_1.vue') },
+  { path: '/ko/project_2', component: httpVueLoader('/en/project_2.vue') },
+  { path: '/ko/project_3', component: httpVueLoader('/en/project_3.vue') },
+  { path: '/ko/project_4', component: httpVueLoader('/en/project_4.vue') },
   
+  { path: '/zh', component: httpVueLoader('/zh/main.vue') },
+  { path: '/zh/about', component: httpVueLoader('/zh/about.vue') },
+  { path: '/zh/business', component: httpVueLoader('/zh/business.vue') },
+  { path: '/zh/contact', component: httpVueLoader('/zh/contact.vue') },
+  { path: '/zh/project', component: httpVueLoader('/zh/project.vue') },
+  { path: '/zh/history', component: httpVueLoader('history.vue') },
+  { path: '/zh/history_detail/:idx', component: httpVueLoader('history_detail.vue') },
+  { path: '/zh/project', component: httpVueLoader('/zh/project.vue') },
+  { path: '/ko/project_1', component: httpVueLoader('project_1.vue') },
+  { path: '/ko/project_2', component: httpVueLoader('project_2.vue') },
+  { path: '/ko/project_3', component: httpVueLoader('project_3.vue') },
+  { path: '/ko/project_4', component: httpVueLoader('project_4.vue') },
+
 ];
 
 // 3. 라우터 인스턴스를 생성하고 `routes` 옵션을 전달하십시오.
@@ -49,6 +76,25 @@ router.onReady(function(){
   console.log("router.onReady");
   console.log("router.onReady");
 });
+router.beforeEach(function(to, from, next){
+  var lang = this.$cookies.get("bluen-lang"); 
+  console.log("라우터 :"+to.path + " : "+lang);
+  if(lang==null)
+  {
+    lang="/ko";
+    this.$cookies.set("bluen-lang",lang); 
+  }
+  if(to.path.indexOf('/ko')==0 ||to.path.indexOf('/en')==0 || to.path.indexOf('/zh')==0)
+  {
+    next();    
+    return;
+  }
+  else
+  {
+    next(lang+to.path);
+  }
+});
+
 //Vue.use( Script2);
 // 4. 루트 인스턴스를 만들고 마운트하십시오.
 // 라우터 옵션을 라우터에 삽입하여
