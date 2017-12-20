f4413a7536a1fcc9184e4aaba2854777dbc29677
Julian.JUNG <jedilick@pied.kr>
2017-12-20 17:15:35 +0900

    영어 vue.json

---
+++ en/contct.vue
@@ -0,0 +1,133 @@
+﻿<template>
+<div v-html="">
+<!-- Content
+================================================== -->
+
+<!-- 960 Container -->
+<div class="container">
+
+	<div class="sixteen columns">
+	
+		<!-- Page Title -->
+		<div id="page-title">
+			<h2>Contact us</h2>
+			<div id="bolded-line"></div>
+		</div>
+		<!-- Page Title / End -->
+
+	</div>
+</div>
+<!-- 960 Container / End -->
+
+
+<!--  960 Container Start -->
+<div class="container">
+
+	<!-- Text -->
+	<div class="sixteen columns">
+		<p>BLUE is a symbol of companies that provide reliable technology for water sources and provide reliable technology.</p>
+		<p>It is the mission of Global Water Treatment Company, which creates a green environment with innovative technologies that coexist with humans and nature.</p>
+		<br />
+	</div>
+
+	<!-- Contact Form -->
+	<div class="twelve columns">
+		<div class="headline no-margin"><h4>Contact Form</h4></div>
+		
+		<div class="form-spacer"></div>
+		
+		<!-- Success Message -->
+		<div class="success-message">
+			<div class="notification success closeable">
+				<p><span>Success!</span> 블루앤 프로젝트에 관심을 보내주셔서 감사합니다. Thank you for your interest bout BLUE N project.</p>
+			</div>
+		</div>
+
+		<!-- Form -->
+		<div id="contact-form">
+			<form method="post" action="contact.php">
+				
+				<div class="field">
+					<label>Name:</label>
+					<input type="text" name="name" class="text" />
+				</div>
+				
+				<div class="field">
+					<label>Email: <span>*</span></label>
+					<input type="text" name="email" class="text" />
+				</div>
+				
+				<div class="field">
+					<label>Message: <span>*</span></label>
+					<textarea name="message" class="text textarea" ></textarea>
+				</div>
+				
+				<div class="field">
+					<input type="button" id="send" value="Send Message"/>
+					<div class="loading"></div>
+				</div>
+				
+			</form>
+		</div>
+
+</div>
+	
+	<!-- Contact Details -->
+	<div class="four columns google-map">
+
+		<div class="headline no-margin"><h4>Company Location</h4>
+			<!-- Text -->
+	    			<p>H.Q. Office :</p> <p>JEIPI. 1, Gangjinsandan-ro 1-gil, Seongjeon-myeon, Gangjin-gun, Jeollanam-do </p>
+			<p>Seoul R&D Centre Branch :</p> <p>#601(e-TechValley) 128, Wonhyo-ro, Yongsan-gu, Seoul </p>
+			<br />
+		</div>
+			
+		<!-- Google Maps -->
+		<div id="googlemaps" class="google-map google-map-full" style="height:250px"></div>
+
+		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+		<script src="js/jquery.gmap.min.js"></script>
+		
+		<script type="text/javascript">
+		jQuery('#googlemaps').gMap({
+			maptype: 'ROADMAP',
+			scrollwheel: false,
+			zoom: 13,
+			markers: [
+				{
+					address: '(재단)전라남도환경산업진흥원 전라남도 강진군 성전면 송학리', // 본점주소 전라남도 강진군 성전면 강진산단로 1길 1, 216호
+					html: '',
+					popup: false,
+				}
+
+
+			],
+			
+		});
+		</script>
+		
+	</div>
+	
+	<!-- Contact Details -->
+	<div class="four columns">
+		<br>
+		<div class="headline low-margin"><h4>Social</h4></div>
+
+			<!-- Social Icons -->
+			<ul class="social-icons">
+				<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+				<li><a class="dribbble" href="#"><i class="icon-dribbble"></i></a></li>
+				<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+				<li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+				<li><a class="pinterest" href="#"><i class="icon-pinterest"></i></a></li>
+			</ul>
+		
+	</div>
+
+</div>
+<!-- End 960 Container -->
+
+</div>
+<!-- Wrapper / End -->
+</div>
+</template>
